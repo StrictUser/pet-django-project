f3fec1a20b43a0ea435245580e83b304a500fb13
Vitalii <vitalii@Vit.local>
2020-03-02 00:23:08 +0200

    Created new app inside the Django project. Added 2 classes for DB. Create migrations for new classes.

---
+++ django_proj/mysite/osint/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+# Create your models here.
+class Category(models.Model):
+    """Category of OSINT services"""
+
+    name = models.CharField("Name of the category", max_length=150)
+    description = models.TextField("Short description of the category")
+    url = models.SlugField(max_length=200, unique=True)
+    img = models.ImageField("Image for category", upload_to="mages/")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "OSINT category"
+        verbose_name_plural = "OSINT category"
+
+
+class OsintServices(models.Model):
+    """Links to the OSINT services with short description"""
+
+    name = models.CharField("Name of the service", max_length=150)
+    description = models.TextField("Short description of the category")
+    url = models.SlugField(max_length=200, unique=True)
+    category = models.ForeignKey(Category,
+                                 verbose_name="OSINT category",
+                                 on_delete=models.SET_NULL,
+                                 null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "OSINT service"
+        verbose_name_plural = "OSINT service"
